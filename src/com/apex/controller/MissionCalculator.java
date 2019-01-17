81e09a22e17092ed18910b900fa0675154981ed4
SevenAngels <aritchie05@gmail.com>
2019-01-17 17:03:18 -0600

    Add accessors for MissionCalculator fields

--- src/com/apex/controller/MissionCalculator.java
+++ src/com/apex/controller/MissionCalculator.java
@@ -40,6 +40,62 @@ public class MissionCalculator {
     }
 
     private void optimizeSearchSpace() {
+    //TODO: implement search space optimization
+    }
 
+    public Voyage getVoyage() {
+        return voyage;
+    }
+
+    public void setVoyage(Voyage voyage) {
+        this.voyage = voyage;
+    }
+
+    public ArrayList<CrewMember> getCrewRoster() {
+        return crewRoster;
+    }
+
+    public void setCrewRoster(ArrayList<CrewMember> crewRoster) {
+        this.crewRoster = crewRoster;
+    }
+
+    public ArrayList<Captain> getCaptains() {
+        return captains;
+    }
+
+    public void setCaptains(ArrayList<Captain> captains) {
+        this.captains = captains;
+    }
+
+    public ArrayList<Bow> getBows() {
+        return bows;
+    }
+
+    public void setBows(ArrayList<Bow> bows) {
+        this.bows = bows;
+    }
+
+    public ArrayList<DeckItem> getDeckItems() {
+        return deckItems;
+    }
+
+    public void setDeckItems(ArrayList<DeckItem> deckItems) {
+        this.deckItems = deckItems;
+    }
+
+    public ArrayList<Hull> getHulls() {
+        return hulls;
+    }
+
+    public void setHulls(ArrayList<Hull> hulls) {
+        this.hulls = hulls;
+    }
+
+    public Rudder getRudder() {
+        return rudder;
+    }
+
+    public void setRudder(Rudder rudder) {
+        this.rudder = rudder;
     }
 }
